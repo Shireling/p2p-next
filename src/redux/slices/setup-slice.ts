cd7f8e0c053883e7c0ccc59ba5924d8557ae4cb6
Shireling <ckramer787@gmail.com>
2023-11-10 15:53:14 -0600

    Finished setup account state propogation

--- src/redux/slices/setup-slice.ts
+++ src/redux/slices/setup-slice.ts
@@ -6,11 +6,33 @@ type InitialState = {
 
 type SetupState = {
   step: string
+  data?: SetupData
+}
+
+type SetupData = {
+  firstName: string
+  lastName: string
+  addressLineOne: string
+  addressLineTwo: string | undefined
+  city: string
+  stateProv: string
+  zip: string
+  phone: string
 }
 
 const initialState = {
   value: {
-    step: '1'
+    step: '1',
+    data: {
+      firstName: '',
+      lastName: '',
+      addressLineOne: '',
+      addressLineTwo: '',
+      city: '',
+      stateProv: '',
+      zip: '',
+      phone: ''
+    }
   } as SetupState
 } as InitialState
 
@@ -21,12 +43,21 @@ export const setupFlow = createSlice({
     setStep: (state, action: PayloadAction<string>) => {
       return {
         value: {
-          step: action.payload
+          step: action.payload,
+          data: state.value.data
+        }
+      }
+    },
+    setData: (state, action: PayloadAction<SetupData>) => {
+      return {
+        value: {
+          step: state.value.step,
+          data: action.payload
         }
       }
     }
   }
 })
 
-export const {setStep} = setupFlow.actions
+export const {setStep, setData} = setupFlow.actions
 export default setupFlow.reducer
